8fb850482c2222a9d21ea8c53ed4e1acd087f28e
chenchaoyun <chenchaoyun@rainbowcn.com>
2024-03-24 15:52:03 +0800

    feta:rinkeby network

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,6 +1,17 @@
-require('@nomicfoundation/hardhat-toolbox')
-
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
-    solidity: '0.8.24',
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+    },
+    solidity: "0.8.24",
 }
