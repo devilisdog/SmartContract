49f329200e6f339d181905e01fea5e7438b60dbd
0x10 <378772004@qq.com>
2025-05-05 20:50:08 +0800

    feat:修改接口

--- shopkeeper/components/common/yc-common-BettingInfo/yc-Bettinglnfo-imgCard/yc-Bettinglnfo-imgCard.vue
+++ shopkeeper/components/common/yc-common-BettingInfo/yc-Bettinglnfo-imgCard/yc-Bettinglnfo-imgCard.vue
@@ -3,7 +3,7 @@
 		
 		
 		<view style="font-weight:600;margin:10rpx 0rpx;">
-			<text style="font-size:32rpx;">彩票照片（最多上传3张）</text>
+			<text style="font-size:32rpx;">（最多上传3张）</text>
 			
 		</view>
 		<uni-notice-bar show-icon :scrollable="false" background-color="white" speed="35" text="为避免店铺被恶意投诉,拍票时请务必遮挡票面密码,站点号,地址,二维码等信息" color="#ff0000"/>
@@ -42,14 +42,14 @@
 		uni.showLoading({title:'上传中'})
 		// console.log(counter.baseUrl)
 		uni.uploadFile({
-		    url:counter.baseUrl+'/api/common.Common/upload',
+		    url:counter.baseUrl+'/api/common.Common/setImg',
 		    header:{server:1, "ba-user-token":counter.access_token},
 		    filePath:e.tempFilePaths[0],
 		    name:'file',
 			formData:{"type":'TicketingImg',"name":counter.orderData.order_id},
 		    success(res){
 				uni.hideLoading()
-				counter.orderData.ReturnImages=res.data.data.file
+				counter.orderData.ReturnImages=JSON.parse(res.data).data
 				uni.showToast({
 					title:'图片上传成功~',
 					icon:'none',
